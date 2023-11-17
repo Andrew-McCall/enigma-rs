620bd033ca7fe8acbc7a22b014287f92fd916440
Work <andrewdavidmccall@hotmail.com>
2023-11-17 11:41:20 +0000

    --message args created

--- src/enigma/main.rs
+++ src/enigma/main.rs
@@ -1,3 +1,5 @@
+mod enigma_converts;
+
 fn main() {
     let args = std::env::args().collect::<Vec<String>>();
 
@@ -5,34 +7,69 @@ fn main() {
 
     let mut rotors = Vec::new();
 
+    let mut message = None;
+
     let mut i = 0;
     while i < args.len() {
         let current_argument = &args[i];
 
-        if current_argument == "-rotor" || current_argument == "-r" {
+        if current_argument == "--rotor" || current_argument == "-r" {
             if i + 3 >= args.len() {
-                println!(
-                    "Missing arguments for rotor {}.\n -rotor ENCODING TURNOVER POSITION",
+                panic!(
+                    "Missing arguments for rotor {}.\n --rotor ENCODING TURNOVER POSITION",
                     rotors.len() + 1
                 );
-                return;
             }
 
             match arg_to_rotor(&args[i + 1], &args[i + 2], &args[i + 3]) {
                 Ok(rotor) => rotors.push(rotor),
                 Err(e) => {
-                    println!("Error parsing rotor {}.\n{}", rotors.len() + 1, e);
-                    return;
+                    panic!("Error parsing rotor {}.\n{}", rotors.len() + 1, e);
                 }
             }
 
             i += 3;
+        } else if current_argument == "--message" || current_argument == "-m" {
+            if message.is_some() {
+                panic!("--message can only be used once.");
+            }
+
+            if i + 1 >= args.len() {
+                panic!("Missing argument for message.\n --message \"MESSAGE\"");
+            }
+
+            let remaining_args = &args[(i + 1)..];
+
+            let input = remaining_args.join(" ");
+            let mut first_quote = None;
+            let mut last_quote = None;
+            input.char_indices().for_each(|c| {
+                if c.1 == '"' {
+                    if first_quote.is_none() {
+                        first_quote = Some(c.0);
+                    } else {
+                        last_quote = Some(c.0);
+                    }
+                }
+            });
+
+            if first_quote.is_none() && last_quote.is_none() {
+                message = Some(args[i + 1].clone());
+                i += 1;
+            } else if first_quote.is_none() && last_quote.is_none() {
+                message = Some(input[(first_quote.unwrap() + 1)..last_quote.unwrap()].to_string());
+                i += 1;
+            } else {
+                panic!("Invalid message format. Must be --message \"MESSAGE TEXT\"");
+            }
         }
 
         i += 1;
     }
 
     println!("Rotors: {:?}", rotors);
+
+    let enigma = Enigma { rotors };
 }
 
 fn arg_to_rotor(encoding: &String, turnover: &String, position: &String) -> Result<Rotor, String> {
